c675db4f5491dd428728162ad8de1c478535c336
unknown <wallethan528@gail.com>
2023-06-02 15:21:49 -0400

    js added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 console.log("JS file connected");
 
-const badge = document.querySelector("#diamond");
+const diamond = document.querySelector("#diamond");
 
 function logId() {
     console.log(this.id);
@@ -8,3 +8,48 @@ function logId() {
 
 
 diamond.addEventListener("click", logId);
+
+const badge = document.querySelector("#badge");
+
+function logId() {
+    console.log(this.id);
+}
+
+
+badge.addEventListener("click", logId);
+
+const icon = document.querySelector("#icon");
+
+function logId() {
+    console.log(this.id);
+}
+
+
+icon.addEventListener("click", logId);
+
+const tri = document.querySelector("#tri");
+
+function logId() {
+    console.log(this.id);
+}
+
+
+tri.addEventListener("click", logId);
+
+const sun = document.querySelector("#Sun");
+
+function logId() {
+    console.log(this.id);
+}
+
+
+sun.addEventListener("click", logId);
+
+const messdice = document.querySelector("#messdice");
+
+function logId() {
+    console.log(this.id);
+}
+
+
+messdice.addEventListener("click", logId);
